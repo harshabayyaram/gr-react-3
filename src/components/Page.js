f6537ac241da8c3770045b3ede625e5375808d1c
Harsha <bayyaramharsha@gmail.com>
2023-08-09 16:04:48 +0530

    completed assignment

---
+++ src/components/Page.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Card from "./Card";
+
+function Page() {
+  return (
+    <div className="flex">
+      <Card />
+      <Card/>
+      <Card />
+      <Card/>
+
+    </div>
+  );
+}
+
+export default Page;
